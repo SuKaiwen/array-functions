780ca28946a1381bcdc88c2f96333303c91eb19f
Kevin Su <kevinsu@outlook.co.nz>
2021-10-17 12:57:00 +1300

    filter

--- src/App.js
+++ src/App.js
@@ -37,6 +37,8 @@ items.forEach(item => {
 })
 
 // Map
+// This function creates new array with the results of calling
+// a function for every array element
 const products = [
   {
     name:"laptop",
@@ -55,11 +57,74 @@ const products = [
   }
 ];
 
+// In this map example: we create a new array by looping 
+// through the old one. We initialise each array item with
+// - The name of the item
+// - The sum of the price and count
 const total = products.map(item => ({
   name: item.name,
   total: item.price * item.count
 }));
 console.log(total);
+
+// Array filter
+// Creates a new array with the items of the old array
+// which satisfies a callback function. If false value = removed.
+const filterNums = [1,2,3,4,5,6]
+
+// Example 1: get all the numbers that are even...
+// We are calling the isEven function on every element 
+const even = filterNums.filter(isEven);
+
+console.log(even);
+
+// Basic function for is even
+function isEven(value){
+  return value % 2 === 0;
+}
+
+// Example 2: remove all young users
+const people = [
+  {
+    name: 'Kevin',
+    age: 19
+  },
+  {
+    name: 'John',
+    age: 25
+  },
+  {
+    name: 'Joe',
+    age: 16
+  },
+  {
+    name: 'Smith',
+    age: 50
+  },
+  {
+    name: 'Gordon',
+    age: 12
+  }
+]
+
+// We can use an arrow function to filter
+// For every person (item) in the people array
+// We check if the age of the item is over/equal to 18
+// If false condition = remove from array
+const oldPeople = people.filter(person => person.age >= 18);
+console.log(oldPeople);
+
+// Example 3: remove duplicates
+const dupeNums = [1,2,6,3,5,2,4,2,5,3,64,1,2,5]
+
+const nonDupe = dupeNums.filter((value, index, arr) => {
+  return arr.indexOf(value) === index;
+})
+
+console.log(nonDupe);
+
+
+
 
 function App() {
 
