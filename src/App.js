c3cbabcc9b4a20444da00c54f2855d324af28311
Kevin Su <kevinsu@outlook.co.nz>
2021-10-17 12:41:47 +1300

    forEach

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+const numbers = [1,2,3,4,5,6,7,8,9,10];
+
+// For each:
+numbers.forEach((item, index, arr) => {
+  // Do something
+  // Index = some index in the array
+  // Item = item corresponding to that index
+  // Arr = the array ([1,2,3...])
+  console.log("Index " + index + ": item " + item);
+})
+
+// Example: adding sum
+let sum = 0;
+
+numbers.forEach((item) => {
+  sum += item;
+})
+
+// 55
+console.log(sum);
+
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Array functions</h1>
     </div>
   );
 }
